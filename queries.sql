58253fa1ac506c76b7d353e7e90c1ae856469921
Aigul9 <46625461+Aigul9@users.noreply.github.com>
2024-10-03 00:52:24 +0300

    num changes, difficulty and total diff.sql

--- queries.sql
+++ queries.sql
@@ -49,18 +49,38 @@ p = shortestPath((from)-[*..150]-(to))
 WHERE from.id = 1 AND to.id = 39
 RETURN p
 
---с минимальной разницей в количестве этажей
+--с минимальным количеством пересадок, сложностью пересадок и разницей в количестве этажей
 MATCH p = SHORTEST 3 (from: Floor)-[r*]-(to: Floor)
-WHERE from.id = 26 AND to.id = 14
-RETURN p, length(p) AS result, reduce(total = 0, r IN relationships(p) | total + r.diff) AS total_diff
-ORDER BY total_diff
-
---с минимальным количеством пересадок
-MATCH p = SHORTEST 3 (from: Floor)-[r*]-(to: Floor)
-WHERE from.id = 4 AND to.id = 41
-WITH p, length(p) AS result, reduce(total = 0, r IN relationships(p) | total + r.diff) AS total_diff, reduce(total = [], r IN relationships(p) | total + r.hallway) AS hallways
-RETURN p, result, total_diff, hallways, reduce(changes = 0, i IN range(1, size(hallways) - 1) | changes + CASE WHEN hallways[i] <> hallways[i - 1] THEN 1 ELSE 0 END) AS num_changes
-ORDER BY num_changes, total_diff
+WHERE from.id = 36 AND to.id = 24
+WITH
+    p,
+    length(p) AS result,
+    reduce(total = 0, r IN relationships(p) | total + r.diff) AS total_diff,
+    reduce(total = [], r IN relationships(p) | total + r.hallway) AS hallways
+RETURN
+    p,
+    result,
+    hallways,
+    reduce(
+        changes = 0,
+        i IN range(1, size(hallways) - 1) |
+        changes + CASE
+            WHEN hallways[i] <> hallways[i-1] THEN 1
+            ELSE 0
+        END
+    ) AS num_changes,
+    reduce(
+        changes = 0,
+        i IN range(1, size(hallways) - 1) |
+        changes + CASE
+            WHEN hallways[i] = 'C' and hallways[i-1] <> 'C' THEN 1
+            WHEN hallways[i] <> 'C' and hallways[i-1] = 'C' THEN 1
+            WHEN hallways[i] <> hallways[i-1] THEN 0.5
+            ELSE 0
+        END
+    ) AS changes_difficulty,
+    total_diff
+ORDER BY num_changes, changes_difficulty, total_diff
 
 --выбрать вершину со всеми связями
 MATCH (n WHERE n.id = 27)-[r]-(m)
